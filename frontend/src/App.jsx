c236449589806869cff92f84243e6ee0ff6991de
jyotiraditya <jyotiraditya24@gmail.com>
2024-06-14 01:07:45 +0530

    Login page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="h-screen flex items-center justify-center p-4">
-      <Login/>
+      <SignUp />
     </div>
   );
 }
