bf5b474190f4269750b03d1529c0c1d5f028f78e
jyotiraditya <jyotiraditya24@gmail.com>
2024-06-17 18:39:24 +0530

    UI DONE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
 
+
 const App = () => {
   return (
     <div className="h-screen flex items-center justify-center p-4">
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SideBar from "../../components/Sidebar";
+import MessageContainer from "../../components/MessageContainer/MessageContainer";
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-opacity-0 backdrop-blur-lg backdrop-filter bg-clip-border p-5">
       <SideBar />
-      {/* <MessageContainer /> */}
+      <MessageContainer />
     </div>
   );
 };
